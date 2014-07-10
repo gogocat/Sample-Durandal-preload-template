db5dbfba2af9bb0844f5eb6a3d570f47342ca111
gogocat <adamchow2326@yahoo.com.au>
2014-07-11 00:00:34 +1000

    added durandal samples using preload templates

---
+++ durandal-samples/app/main.js
@@ -0,0 +1,30 @@
+﻿requirejs.config({
+    paths: {
+        'text': 'vendor/text',
+        'durandal':'vendor/durandal/js',
+        'plugins' : 'vendor/durandal/js/plugins',
+        'transitions' : 'vendor/durandal/js/transitions'
+    }
+});
+
+define('jquery', function(){ return jQuery; });
+define('knockout', ko);
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    system.debug(true);
+
+    app.title = 'Durandal Samples';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true,
+        widget: {
+            kinds: ['expander']
+        }
+    });
+
+    app.start().then(function () {
+        viewLocator.useConvention();
+        app.setRoot('shell');
+    });
+});
